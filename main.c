17c04fd50e3cd2c4d1115d1faaa222d19afbd70b
Clement Albert <clement.albert@epfl.ch>
2021-04-18 20:43:32 +0200

    TP 5 marche en meme temps que modif detection couleur

--- main.c
+++ main.c
@@ -101,59 +101,59 @@ int main(void)
 
 		/* Infinite loop. */
 		while (1) {
-//	#ifdef SEND_FROM_MIC
-//			//waits until a result must be sent to the computer
-//			wait_send_to_computer();
-//	#ifdef DOUBLE_BUFFERING
-//			//we copy the buffer to avoid conflicts
-//			arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
-//			SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
-//	#else
-//			SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
-//	#endif  /* DOUBLE_BUFFERING */
-//	#else
-//			//time measurement variables
-//			volatile uint16_t time_fft = 0;
-//			volatile uint16_t time_mag  = 0;
-//
-//			float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
-//			float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
-//
-//			uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
-//
-//			if(size == FFT_SIZE){
-//				/*
-//				*   Optimized FFT
-//				*/
-//
-//				chSysLock();
-//				//reset the timer counter
-//				GPTD12.tim->CNT = 0;
-//
-//				doFFT_optimized(FFT_SIZE, bufferCmplxInput);
-//
-//				time_fft = GPTD12.tim->CNT;
-//				chSysUnlock();
-//
-//				/*
-//				*   End of optimized FFT
-//				*/
-//
-//
-//				chSysLock();
-//				//reset the timer counter
-//				GPTD12.tim->CNT = 0;
-//
-//				arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
-//
-//				time_mag = GPTD12.tim->CNT;
-//				chSysUnlock();
-//
-//				SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
-//				//chprintf((BaseSequentialStream *) &SDU1, "time fft = %d us, time magnitude = %d us\n",time_fft, time_mag);
-//
-//			}
-//	#endif  /* SEND_FROM_MIC */
+	#ifdef SEND_FROM_MIC
+			//waits until a result must be sent to the computer
+			wait_send_to_computer();
+	#ifdef DOUBLE_BUFFERING
+			//we copy the buffer to avoid conflicts
+			arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
+			SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
+	#else
+			SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
+	#endif  /* DOUBLE_BUFFERING */
+	#else
+			//time measurement variables
+			volatile uint16_t time_fft = 0;
+			volatile uint16_t time_mag  = 0;
+
+			float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
+			float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
+
+			uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
+
+			if(size == FFT_SIZE){
+				/*
+				*   Optimized FFT
+				*/
+
+				chSysLock();
+				//reset the timer counter
+				GPTD12.tim->CNT = 0;
+
+				doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+
+				time_fft = GPTD12.tim->CNT;
+				chSysUnlock();
+
+				/*
+				*   End of optimized FFT
+				*/
+
+
+				chSysLock();
+				//reset the timer counter
+				GPTD12.tim->CNT = 0;
+
+				arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
+
+				time_mag = GPTD12.tim->CNT;
+				chSysUnlock();
+
+				SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
+				//chprintf((BaseSequentialStream *) &SDU1, "time fft = %d us, time magnitude = %d us\n",time_fft, time_mag);
+
+			}
+	#endif  /* SEND_FROM_MIC */
 		}
 }
 
