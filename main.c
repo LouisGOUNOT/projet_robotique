dd3b0f3a3bfa825e40b28e063296d1ac6abc8c3e
Clement Albert <clement.albert@epfl.ch>
2021-04-18 20:09:48 +0200

    pasuf

--- main.c
+++ main.c
@@ -12,6 +12,13 @@
 #include <camera/po8030.h>
 #include <chprintf.h>
 
+#include <audio/microphone.h>
+
+#include <audio_processing.h>
+#include <fft.h>
+#include <communications.h>
+#include <arm_math.h>
+
 #include <pi_regulator.h>
 #include <process_image.h>
 
@@ -33,6 +40,29 @@ static void serial_start(void)
 
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
+//uncomment to send the FFTs results from the real microphones
+//#define SEND_FROM_MIC
+
+//uncomment to use double buffering to send the FFT to the computer
+#define DOUBLE_BUFFERING
+
+
+static void timer12_start(void){
+    //General Purpose Timer configuration
+    //timer 12 is a 16 bit timer so we can measure time
+    //to about 65ms with a 1Mhz counter
+    static const GPTConfig gpt12cfg = {
+        1000000,        /* 1MHz timer clock in order to measure uS.*/
+        NULL,           /* Timer callback.*/
+        0,
+        0
+    };
+
+    gptStart(&GPTD12, &gpt12cfg);
+    //let the timer count to max value
+    gptStartContinuous(&GPTD12, 0xFFFF);
+}
+
 
 int main(void)
 {
@@ -48,19 +78,85 @@ int main(void)
     //starts the camera
     dcmi_start();
 	po8030_start();
+    //starts timer 12
+    timer12_start();
 	//inits the motors
 	motors_init();
 
 	//stars the threads for the pi regulator and the processing of the image
 //	pi commenté pour tester la detection de couleur uniquement
 //	pi_regulator_start();
-	process_image_start();
+	//process_image_start();
 
-    /* Infinite loop. */
-    while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(1000);
-    }
+    //temp tab used to store values in complex_float format
+    //needed bx doFFT_c
+    static complex_float temp_tab[FFT_SIZE];
+    //send_tab is used to save the state of the buffer to send (double buffering)
+    //to avoid modifications of the buffer while sending it
+    static float send_tab[FFT_SIZE];
+
+	#ifdef SEND_FROM_MIC
+		//starts the microphones processing thread.
+		//it calls the callback given in parameter when samples are ready
+		mic_start(&processAudioData);
+	#endif  /* SEND_FROM_MIC */
+
+		/* Infinite loop. */
+		while (1) {
+	#ifdef SEND_FROM_MIC
+			//waits until a result must be sent to the computer
+			wait_send_to_computer();
+	#ifdef DOUBLE_BUFFERING
+			//we copy the buffer to avoid conflicts
+			arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
+			SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
+	#else
+			SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
+	#endif  /* DOUBLE_BUFFERING */
+	#else
+			//time measurement variables
+			volatile uint16_t time_fft = 0;
+			volatile uint16_t time_mag  = 0;
+
+			float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
+			float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
+
+			uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
+
+			if(size == FFT_SIZE){
+				/*
+				*   Optimized FFT
+				*/
+
+				chSysLock();
+				//reset the timer counter
+				GPTD12.tim->CNT = 0;
+
+				doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+
+				time_fft = GPTD12.tim->CNT;
+				chSysUnlock();
+
+				/*
+				*   End of optimized FFT
+				*/
+
+
+				chSysLock();
+				//reset the timer counter
+				GPTD12.tim->CNT = 0;
+
+				arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
+
+				time_mag = GPTD12.tim->CNT;
+				chSysUnlock();
+
+				SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
+				//chprintf((BaseSequentialStream *) &SDU1, "time fft = %d us, time magnitude = %d us\n",time_fft, time_mag);
+
+			}
+	#endif  /* SEND_FROM_MIC */
+		}
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
