d26e762c3afcb56e6a2f1602f9fb0c6edd79c488
louis <louis.gounot@epfl.ch>
2021-05-04 13:20:45 +0200

    main modifié

--- main.c
+++ main.c
@@ -9,29 +9,35 @@
 #include <usbcfg.h>
 #include <main.h>
 #include <motors.h>
+#include "sensors/proximity.h"
+#include <spi_comm.h>
 #include <camera/po8030.h>
 #include <chprintf.h>
 
 #include <pi_regulator.h>
 #include <process_image.h>
+#include <obstacle.h>
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
+void SendUint8ToComputer(uint8_t* data, uint16_t size)
 {
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+    chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+    chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+    chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
 }
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
 static void serial_start(void)
 {
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
+    static SerialConfig ser_cfg = {
+        115200,
+        0,
+        0,
+        0,
+    };
 
-	sdStart(&SD3, &ser_cfg); // UART3.
+    sdStart(&SD3, &ser_cfg); // UART3.
 }
 
 int main(void)
@@ -40,28 +46,42 @@ int main(void)
     halInit();
     chSysInit();
     mpu_init();
+      /** Inits the Inter Process Communication bus. */
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
 
     //starts the serial communication
     serial_start();
     //start the USB communication
     usb_start();
+    //Start the SPI Communication
+    spi_comm_start();
+
     //starts the camera
     dcmi_start();
-	po8030_start();
-	//inits the motors
-	motors_init();
+    po8030_start();
+    //inits the motors
+    motors_init();
 
-	//stars the threads for the pi regulator and the processing of the image
-	pi_regulator_start();
-	process_image_start();
+    //stars the threads for the pi regulator and the processing of the image
+    pi_regulator_start();
+    process_image_start();
+
+    //start the threads for the detector of proximity
+    proximity_start();
+
+    obstacle_start();
+
+    //static uint16_t proxi[NUMSENSOR];
+    //static int distance[NUMSENSOR];
 
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
+
+
         chThdSleepMilliseconds(1000);
     }
-}
 
+}
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
